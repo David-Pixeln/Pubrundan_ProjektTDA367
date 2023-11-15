f77916bbd57c8c00662f8bf697f328f0f3cee8e1
Christian <chrille_0313@hotmail.com>
2023-11-15 01:05:01 +0100

    Spacing

--- Frontend/app/(tabs)/friends.tsx
+++ Frontend/app/(tabs)/friends.tsx
@@ -3,9 +3,9 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 
 export default function Friends() {
-    return (
-        <SafeAreaView>
-            <Text>Friends</Text>
-        </SafeAreaView>
-    );
+  return (
+    <SafeAreaView>
+      <Text>Friends</Text>
+    </SafeAreaView>
+  );
 }
