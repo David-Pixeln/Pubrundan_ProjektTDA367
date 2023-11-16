86b6830abb21115fa5095f4301ffa55dd87d3af6
David-Pixeln <kalmarkiwi03@gmail.com>
2023-11-16 14:12:25 +0100

    Added a new test and the class Pub

--- src/main/java/PubCrawl.java
+++ src/main/java/PubCrawl.java
@@ -1,2 +1,10 @@
+import jdk.jshell.spi.ExecutionControl;
+
 public class PubCrawl {
+
+
+    public boolean allPubsOpen(){
+        throw new ExecutionControl.NotImplementedException("Will be implement in release 3.5");
+    };
+
 }
--- src/test/java/PubCrawlTest.java
+++ src/test/java/PubCrawlTest.java
@@ -1,17 +1,25 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalTime;
+import com.google.android.gms.maps.model.LatLng;
 
 public class PubCrawlTest {
 
     @Test
     void testAllPubsOpenTrue() {
+        Pub myPub1 = new Pub("namePub1", LocalTime.of(15,00), LocalTime.of(03,00), Position());
+        Pub myPub2 = new Pub("namePub2", LocalTime.of(15,00), LocalTime.of(01,00), Position());
+        PubCrawl myPubCrawl = new PubCrawl("name");
+        myPubCrawl.addPub(myPub1);
+        myPubCrawl.addPub(myPub2);
+        assertEquals(myPubCrawl,);
     }
 
     @Test
     void testAddPub() {
         Pub myPub = new Pub();
         PubCrawl myPubCrawl = new PubCrawl("name");
-        myPubCrawl.addPub(MyPub);
+        myPubCrawl.addPub(myPub);
         assertEquals(myPubCrawl.getPubs()[0], MyPub);
     }
 
@@ -19,9 +27,12 @@ public class PubCrawlTest {
     void testRemovePub() {
         Pub myPub = new Pub();
         PubCrawl myPubCrawl = new PubCrawl("name");
-        myPubCrawl.addPub(MyPub);
-        myPubCrawl.removePub(MyPub);
+        myPubCrawl.addPub(myPub);
+        myPubCrawl.removePub(myPub);
         assertEquals(myPubCrawl.getPubs().length(), 0);
     }
+
+    @Test
+    void testGetClosingTime() {}
 
 }
