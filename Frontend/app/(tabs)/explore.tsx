83ec5083e0a150c5ba5ab3f1f82d29f044ba17e8
Christian <chrille_0313@hotmail.com>
2023-11-16 16:26:48 +0100

    Update explore.tsx

--- Frontend/app/(tabs)/explore.tsx
+++ Frontend/app/(tabs)/explore.tsx
@@ -1,11 +1,22 @@
+import { ThemeContext } from "@constants/themes";
+import { useContext } from "react";
 import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-
+import { StyleSheet } from "react-native";
 
 export default function Explore() {
+  const theme = useContext(ThemeContext);
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+  });
+  
+
   return (
-    <SafeAreaView>
-      <Text>Explore</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={{color: theme.colors.text}}>Explore</Text>
     </SafeAreaView>
   );
 }
