d2547d0ea15da56e273e52c0766f35fcc5f38395
Christian <chrille_0313@hotmail.com>
2023-11-23 16:18:46 +0100

    Refactor project

---
+++ Backend/src/main/java/com/Pubrunda/repositories/PubRepository.java
@@ -0,0 +1,8 @@
+package repository;
+
+import com.Pubrunda.Pub;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PubRepository extends JpaRepository<Pub, Long> {
+
+}
