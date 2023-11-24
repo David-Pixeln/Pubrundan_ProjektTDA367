66bf1714d99313bbc5e2f064d96c322a31cb167d
Christian <chrille_0313@hotmail.com>
2023-11-24 03:13:33 +0100

    Update PubRepository.java

--- Backend/src/main/java/com/Pubrunda/repositories/PubRepository.java
+++ Backend/src/main/java/com/Pubrunda/repositories/PubRepository.java
@@ -2,7 +2,9 @@ package com.Pubrunda.repositories;
 
 import com.Pubrunda.models.Pub;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PubRepository extends JpaRepository<Pub, Long> {
 
 }
