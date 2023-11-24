675796e3a5e9c432764e7e9a85995f700eb1b33f
Christian <chrille_0313@hotmail.com>
2023-11-24 11:21:30 +0100

    Update User.java

--- Backend/src/main/java/com/Pubrunda/models/User.java
+++ Backend/src/main/java/com/Pubrunda/models/User.java
@@ -1,13 +1,18 @@
-/*
 package com.Pubrunda.models;
 
-import com.Pubrunda.models.PubCrawl;
 import jakarta.persistence.Entity;
 
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
 
-import java.awt.Image;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.ArrayList;
@@ -17,64 +22,37 @@ import java.util.ArrayList;
 public class User {
     
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
-    private String name;
+
+    @NonNull
+    @Getter
+    @Setter
     private String username;
+
+    @Getter
+    @Setter
+    private String name;
+
+    @Getter
+    @Setter
     private String description;
-    private Image profileImage;
-    private ArrayList<PubCrawl> finishedPubCrawls;
-    private ArrayList<PubCrawl> favouritePubCrawls;
+
+    @Setter
+    private String profileImagePath;
+
 
     public User() {}
 
-    public User(String name, String username, String description, Image profileImage) {
+    public User(String name, @NonNull String username, String description, String profileImagePath) {
         this.name = name;
         this.username = username;
         this.description = description;
-        this.profileImage = profileImage;
-        this.finishedPubCrawls = new ArrayList<PubCrawl>();
-        this.favouritePubCrawls = new ArrayList<PubCrawl>();
+        this.profileImagePath = profileImagePath;
     }
 
-    // write gettters and setters for these
-    public String getName(){
-        return name;
-    }
-    public String getUsername(){
-        return username;
-    }
-    public String getDescription(){
-        return description;
-    }
-    public Image getProfileImage(){
-        return profileImage;
-    }
-    public ArrayList<PubCrawl> getFinishedPubCrawls(){
-        return finishedPubCrawls;
-    }
-    public ArrayList<PubCrawl> getFavouritePubCrawls(){
-        return favouritePubCrawls;
-    }
-    public void setName(String name){
-        this.name = name;
-    }
-    public void setUsername(String username){
-        this.username = username;
-    }
-    public void setDescription(String description){
-        this.description = description;
-    }
-    public void setProfileImage(Image profileImage){
-        this.profileImage = profileImage;
-    }
-    
-    public void addPubCrawlToFavourites(PubCrawl pubCrawl){
-        favouritePubCrawls.add(pubCrawl);
-    }
-    public void removePubCrawlFromFavourites(PubCrawl pubCrawl){
-        favouritePubCrawls.remove(pubCrawl);
+    public BufferedImage getProfileImage() throws IOException {
+        return ImageIO.read(new File(profileImagePath));
     }
 
 }
-*/
