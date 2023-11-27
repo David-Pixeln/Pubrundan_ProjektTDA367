847c2aa93094a2446e42c044c699270133b90591
Ida Åberg <idaaberg0310@gmail.com>
2023-11-27 14:42:13 +0100

    Updated comments module

--- Backend/src/main/java/com/Pubrunda/models/Comment.java
+++ Backend/src/main/java/com/Pubrunda/models/Comment.java
@@ -6,22 +6,22 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
+@RequiredArgsConstructor
 public class Comment {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+    @NonNull
     @Setter
     private String content;
 
     //TODO add user/author
-
-    public Comment (String content) {
-        this.content = content;
-    }
 
 }
