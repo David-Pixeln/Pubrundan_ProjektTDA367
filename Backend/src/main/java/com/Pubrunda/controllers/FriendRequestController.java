60fae2df7a6b556e4eb06023cf6cc686dd1798c7
Marten-Granath <115485279+Marten-Granath@users.noreply.github.com>
2023-11-27 16:10:41 +0100

    Created FriendRequestController and added relevant methods (Read, Create, Delete)

---
+++ Backend/src/main/java/com/Pubrunda/controllers/FriendRequestController.java
@@ -0,0 +1,42 @@
+package com.Pubrunda.controllers;
+
+import com.Pubrunda.exception.ResourceNotFoundException;
+import com.Pubrunda.models.FriendRequest;
+import com.Pubrunda.models.Pub;
+import com.Pubrunda.repositories.FriendRequestRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@RestController
+@RequestMapping("/api/friendRequests")
+public class FriendRequestController {
+
+    private final FriendRequestRepository repository;
+
+    FriendRequestController(FriendRequestRepository repository) {
+        this.repository = repository;
+    }
+
+    // READ
+    @GetMapping("/{friendRequestId}")
+    public FriendRequest getFriendRequestById(@PathVariable long friendRequestId) {
+        return repository.findById(friendRequestId).orElseThrow(() -> new ResourceNotFoundException(friendRequestId));
+    }
+
+    // CREATE
+    @PostMapping("/friendRequests")
+    public FriendRequest createFriendRequest(@RequestBody FriendRequest newFriendRequest) {
+        return repository.save(newFriendRequest);
+    }
+
+    // DELETE
+    @DeleteMapping("/{friendRequestId}")
+    public ResponseEntity<FriendRequest> deleteUser(@PathVariable Long friendRequestId) {
+        FriendRequest existingFriendRequest = repository.findById(friendRequestId).orElseThrow(() -> new ResourceNotFoundException(friendRequestId));
+        repository.delete(existingFriendRequest);
+        return ResponseEntity.ok().build();
+    }
+
+}
