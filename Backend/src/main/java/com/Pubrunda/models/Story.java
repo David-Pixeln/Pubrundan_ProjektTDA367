77de1ade1c213a8d48432ddb3965dd839c9058e6
Ida Åberg <idaaberg0310@gmail.com>
2023-11-27 15:28:15 +0100

    Update Story.java

--- Backend/src/main/java/com/Pubrunda/models/Story.java
+++ Backend/src/main/java/com/Pubrunda/models/Story.java
@@ -1,4 +1,25 @@
 package com.Pubrunda.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+
+@Entity
+@Getter
 public class Story {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Setter
+    @NonNull
+    private String mediaPath;
+
+    //TODO add user
+
+    public Story() {}
 }
