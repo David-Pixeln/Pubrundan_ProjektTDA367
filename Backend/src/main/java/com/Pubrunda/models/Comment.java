40dca09fc262458aec078e73b5a7c5658c58236d
Ida Åberg <idaaberg0310@gmail.com>
2023-11-27 14:35:30 +0100

    Completed comment model

--- Backend/src/main/java/com/Pubrunda/models/Comment.java
+++ Backend/src/main/java/com/Pubrunda/models/Comment.java
@@ -1,4 +1,27 @@
 package com.Pubrunda.models;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
 public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @Setter
+    private String content;
+
+    //TODO add user/author
+
+    public Comment (String content) {
+        this.content = content;
+    }
+
 }
