eca0d8e29dec84350b3580935ff613c24baafec4
Christian <chrille_0313@hotmail.com>
2023-11-28 01:32:03 +0100

    Update User.java

--- Backend/src/main/java/com/Pubrunda/models/User.java
+++ Backend/src/main/java/com/Pubrunda/models/User.java
@@ -1,58 +1,28 @@
 package com.Pubrunda.models;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.NonNull;
-import lombok.Setter;
-
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.util.ArrayList;
+import lombok.*;
 
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@RequiredArgsConstructor
 public class User {
-    
+
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @NonNull
-    @Getter
-    @Setter
     private String username;
 
-    @Getter
-    @Setter
-    private String name;
+    @NonNull
+    private String passwordHash;
 
-    @Getter
-    @Setter
-    private String description;
-
-    @Setter
-    private String profileImagePath;
-
-
-    public User() {}
-
-    public User(String name, @NonNull String username, String description, String profileImagePath) {
-        this.name = name;
-        this.username = username;
-        this.description = description;
-        this.profileImagePath = profileImagePath;
-    }
-
-    public BufferedImage getProfileImage() throws IOException {
-        return ImageIO.read(new File(profileImagePath));
-    }
+    @NonNull
+    private String role;
 
 }
