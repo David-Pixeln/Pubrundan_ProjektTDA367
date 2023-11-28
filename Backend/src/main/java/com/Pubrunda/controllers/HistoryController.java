3f0a5de67ee4971643e0832dcb3ba3ced7c7dbec
Ida Åberg <idaaberg0310@gmail.com>
2023-11-28 14:07:03 +0100

    FIxed path from history to histories

--- Backend/src/main/java/com/Pubrunda/controllers/HistoryController.java
+++ Backend/src/main/java/com/Pubrunda/controllers/HistoryController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/history")
+@RequestMapping("/api/histories")
 public class HistoryController {
 
     private final HistoryRepository repository;
