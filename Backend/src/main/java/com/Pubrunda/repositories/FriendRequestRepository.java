e8fcb58a5a76af3c052b8f6ee84ffda1b76b735f
Christian <chrille_0313@hotmail.com>
2023-11-30 02:26:29 +0100

    Remove spacing

--- Backend/src/main/java/com/Pubrunda/repositories/FriendRequestRepository.java
+++ Backend/src/main/java/com/Pubrunda/repositories/FriendRequestRepository.java
@@ -6,5 +6,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FriendRequestRepository extends JpaRepository<FriendRequest, Long> {
-
 }
--- Backend/src/main/java/com/Pubrunda/repositories/LeaderboardRepository.java
+++ Backend/src/main/java/com/Pubrunda/repositories/LeaderboardRepository.java
@@ -6,5 +6,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LeaderboardRepository extends JpaRepository<Leaderboard, Long> {
-
 }
