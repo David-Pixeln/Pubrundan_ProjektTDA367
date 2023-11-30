eaaffd265451b8b3c86810190d42ad9f144a3bc3
Christian <chrille_0313@hotmail.com>
2023-11-30 01:42:03 +0100

    Spacing

--- Backend/src/main/java/com/Pubrunda/repositories/ProfileRepository.java
+++ Backend/src/main/java/com/Pubrunda/repositories/ProfileRepository.java
@@ -1,11 +1,12 @@
 package com.Pubrunda.repositories;
 
 import com.Pubrunda.models.Profile;
-import com.Pubrunda.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProfileRepository extends JpaRepository<Profile, Long>{
+
     Profile findByUserId(long id);
+
 }
