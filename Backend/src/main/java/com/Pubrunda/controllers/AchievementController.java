e6245ec63e1b2712890ae37a595131b0bb26ea12
David-Pixeln <kalmarkiwi03@gmail.com>
2023-12-06 18:11:39 +0100

    Fixed baseurl

--- Backend/src/main/java/com/Pubrunda/controllers/AchievementController.java
+++ Backend/src/main/java/com/Pubrunda/controllers/AchievementController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/achievements")
+@RequestMapping("${api.baseurl}/achievements")
 public class AchievementController {
 
     private final AchievementRepository repository;
