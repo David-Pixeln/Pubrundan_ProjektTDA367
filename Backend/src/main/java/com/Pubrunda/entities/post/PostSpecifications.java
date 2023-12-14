b2279f5cf20df6567898b9ff166ef760937d5584
Christian <chrille_0313@hotmail.com>
2023-12-14 12:00:48 +0100

    Refactor all specifications

--- Backend/src/main/java/com/Pubrunda/entities/post/PostSpecifications.java
+++ Backend/src/main/java/com/Pubrunda/entities/post/PostSpecifications.java
@@ -2,49 +2,29 @@ package com.Pubrunda.entities.post;
 
 import com.Pubrunda.EntitySpecifications;
 import com.Pubrunda.entities.post.dto.request.PostQueryParams;
+import com.Pubrunda.specifications.Specifications;
+import com.Pubrunda.specifications.TimeSpecifications;
+import jakarta.persistence.criteria.Root;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.domain.Specification;
 
-import java.time.LocalDateTime;
 import java.util.Collection;
-import java.util.stream.Stream;
+import java.util.List;
+
 
 @RequiredArgsConstructor
 public class PostSpecifications extends EntitySpecifications<Post> {
 
     private final PostQueryParams postParams;
 
-
     @Override
-    protected Collection<Specification<Post>> getSpecifications() {
-        return Stream.of(
-                byId(postParams.getId()),
-                byAuthorId(postParams.getAuthorId()),
-                byAuthorUsername(postParams.getAuthorUsername()),
-                byTimeAfter((postParams.getAfter())),
-                byTimeBefore(postParams.getBefore())
-        ).toList();
-    }
-
-    public Specification<Post> byId(Long id) {
-        return (root, query, builder) -> id != null ? builder.equal(root.get("id"), id) : null;
-    }
-
-    public Specification<Post> byAuthorId(Long id) {
-        return (root, query, builder) -> id != null ? builder.equal(root.get("author").get("id"), id) : null;
-    }
-
-    public Specification<Post> byAuthorUsername(String username) {
-        return (root, query, builder) -> username != null ? builder.equal(root.get("author").get("username"), username) : null;
-    }
-
-    public Specification<Post> byTimeBefore(LocalDateTime before) {
-        return (root, query, builder) -> before != null ? builder.lessThanOrEqualTo(root.get("createdAt"), before) : null;
-    }
-
-    public Specification<Post> byTimeAfter(LocalDateTime after) {
-        return (root, query, builder) -> after != null ? builder.greaterThanOrEqualTo(root.get("createdAt"), after) : null;
+    protected Collection<Specification<Post>> getSpecifications(Root<Post> root) {
+        return List.of(
+                Specifications.byId(postParams.getAuthorId(), root.get("author").get("id")),
+                Specifications.byString(postParams.getAuthorUsername(), root.get("author").get("username")),
+                TimeSpecifications.byTimeBefore(postParams.getBefore(), root.get("createdAt")),
+                TimeSpecifications.byTimeAfter(postParams.getAfter(), root.get("createdAt"))
+        );
     }
 
 }
-
