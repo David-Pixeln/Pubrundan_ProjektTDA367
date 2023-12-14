c30fabe9a570bd083a0bcbb3de43b6cfff46d29d
Marten-Granath <115485279+Marten-Granath@users.noreply.github.com>
2023-12-14 23:35:51 +0100

    Extended to use JPASpecificationExecutor

--- Backend/src/main/java/com/Pubrunda/entities/pub/PubRepository.java
+++ Backend/src/main/java/com/Pubrunda/entities/pub/PubRepository.java
@@ -1,8 +1,9 @@
 package com.Pubrunda.entities.pub;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PubRepository extends JpaRepository<Pub, Long> {
+public interface PubRepository extends JpaRepository<Pub, Long>, JpaSpecificationExecutor<Pub> {
 }
