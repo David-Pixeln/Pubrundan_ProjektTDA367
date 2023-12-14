512a05d313e537ae7b06d684915b089ceb801970
Ida Åberg <idaaberg0310@gmail.com>
2023-12-14 12:51:09 +0100

    Refactored specifications
    Co-Authored-By: Mårten Granath <115485279+Marten-Granath@users.noreply.github.com>
    Co-Authored-By: Christian Mattsson <47303717+chrille0313@users.noreply.github.com>

--- Backend/src/main/java/com/Pubrunda/entities/review/ReviewSpecification.java
+++ Backend/src/main/java/com/Pubrunda/entities/review/ReviewSpecification.java
@@ -2,14 +2,15 @@ package com.Pubrunda.entities.review;
 
 import com.Pubrunda.EntitySpecifications;
 import com.Pubrunda.entities.review.DTO.request.ReviewQueryParams;
-import com.Pubrunda.entities.user.User;
-import jakarta.persistence.criteria.Predicate;
+import com.Pubrunda.specifications.Specifications;
+import com.Pubrunda.specifications.TimeSpecifications;
+import jakarta.persistence.criteria.Root;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.domain.Specification;
 
-import java.time.LocalDateTime;
 import java.util.Collection;
-import java.util.stream.Stream;
+import java.util.List;
+
 
 @RequiredArgsConstructor
 public class ReviewSpecification extends EntitySpecifications<Review> {
@@ -17,24 +18,13 @@ public class ReviewSpecification extends EntitySpecifications<Review> {
     private final ReviewQueryParams reviewParams;
 
     @Override
-    protected Collection<Specification<Review>> getSpecifications() {
-        return Stream.of(
-                byAuthorId(reviewParams.getAuthorId()),
-                byTimeAfter(reviewParams.getCreatedAt()),
-                byTimeBefore(reviewParams.getCreatedAt())
-                ).toList();
-    }
-
-    public Specification<Review> byAuthorId(Long id) {
-        return (root, query, builder) -> id != null ? builder.equal(root.get("author").get("id"), id) : null;
-    }
-
-    public Specification<Review> byTimeBefore(LocalDateTime createdAt) {
-        return (root, query, builder) -> createdAt != null ? builder.lessThanOrEqualTo(root.get("createdAt"), createdAt) : null;
-    }
-
-    public Specification<Review> byTimeAfter(LocalDateTime createdAt) {
-        return (root, query, builder) -> createdAt != null ? builder.greaterThanOrEqualTo(root.get("createdAt"), createdAt) : null;
+    protected Collection<Specification<Review>> getSpecifications(Root<Review> root) {
+        return List.of(
+                Specifications.byId(reviewParams.getAuthorId(), root.get("author").get("id")),
+                Specifications.byString(reviewParams.getAuthorUsername(), root.get("author").get("username")),
+                TimeSpecifications.byTimeAfter(reviewParams.getAfter(), root.get("createdAt")),
+                TimeSpecifications.byTimeBefore(reviewParams.getBefore(), root.get("createdAt"))
+        );
     }
 
 }
