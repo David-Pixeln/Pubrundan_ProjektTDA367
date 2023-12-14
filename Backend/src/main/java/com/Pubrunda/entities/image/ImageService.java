84881a459f9384a3a4cdfe24043536ba1abdbb27
N0_L0gic <chrille_0313@hotmail.com>
2023-12-14 23:08:09 +0100

    Create ImageService.java

---
+++ Backend/src/main/java/com/Pubrunda/entities/image/ImageService.java
@@ -0,0 +1,14 @@
+package com.Pubrunda.entities.image;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ImageService {
+
+    private final ImageRepository imageRepository;
+
+    
+
+}
