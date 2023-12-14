1f7d89c6f224e245843dd8cdb2ac54a77a4b1f82
Ida Åberg <idaaberg0310@gmail.com>
2023-12-14 12:49:38 +0100

    Fixed ReviewDTO
    Co-Authored-By: Mårten Granath <115485279+Marten-Granath@users.noreply.github.com>
    Co-Authored-By: Christian Mattsson <47303717+chrille0313@users.noreply.github.com>

--- Backend/src/main/java/com/Pubrunda/entities/review/DTO/response/ReviewDTO.java
+++ Backend/src/main/java/com/Pubrunda/entities/review/DTO/response/ReviewDTO.java
@@ -3,10 +3,15 @@ package com.Pubrunda.entities.review.DTO.response;
 import com.Pubrunda.entities.user.dto.response.UserDTO;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class ReviewDTO {
 
     private long id;
     private UserDTO author;
-
+    private String mediaPath;
+    private int rating;
+    private String content;
+    private LocalDateTime createdAt;
 }
