c343ca15b71f9f10cbacf9818710d4f9b3a431aa
Ida Åberg <idaaberg0310@gmail.com>
2023-12-15 11:48:07 +0100

    Update CommentRepository.java
    Co-Authored-By: Mårten Granath <115485279+Marten-Granath@users.noreply.github.com>

--- Backend/src/main/java/com/Pubrunda/entities/comment/CommentRepository.java
+++ Backend/src/main/java/com/Pubrunda/entities/comment/CommentRepository.java
@@ -1,8 +1,9 @@
 package com.Pubrunda.entities.comment;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CommentRepository extends JpaRepository<Comment, Long> {
+public interface CommentRepository extends JpaRepository<Comment, Long>, JpaSpecificationExecutor<Comment> {
 }
