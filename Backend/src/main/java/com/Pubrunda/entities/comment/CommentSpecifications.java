e962537065b342147593f83e5234f26aa9f675e8
Ida Åberg <idaaberg0310@gmail.com>
2023-12-15 11:48:47 +0100

    Created comment specifications from ground up
    Co-Authored-By: Mårten Granath <115485279+Marten-Granath@users.noreply.github.com>

--- Backend/src/main/java/com/Pubrunda/entities/comment/CommentSpecifications.java
+++ Backend/src/main/java/com/Pubrunda/entities/comment/CommentSpecifications.java
@@ -1,4 +1,30 @@
 package com.Pubrunda.entities.comment;
 
-public class CommentSpecifications {
+import com.Pubrunda.EntitySpecifications;
+import com.Pubrunda.entities.comment.dto.request.CommentQueryParams;
+import com.Pubrunda.entities.post.Post;
+import com.Pubrunda.entities.post.dto.request.PostQueryParams;
+import com.Pubrunda.specifications.Specifications;
+import com.Pubrunda.specifications.TimeSpecifications;
+import jakarta.persistence.criteria.Root;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.util.Collection;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class CommentSpecifications extends EntitySpecifications<Comment> {
+
+    private final CommentQueryParams commentParams;
+
+    @Override
+    protected Collection<Specification<Comment>> getSpecifications(Root<Comment> root) {
+        return List.of(
+                Specifications.byId(commentParams.getAuthorId(), root.get("author").get("id")),
+                Specifications.byString(commentParams.getAuthorUsername(), root.get("author").get("username")),
+                TimeSpecifications.byTimeBefore(commentParams.getBefore(), root.get("createdAt")),
+                TimeSpecifications.byTimeAfter(commentParams.getAfter(), root.get("createdAt"))
+        );
+    }
 }
